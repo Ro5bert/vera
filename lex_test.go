51865a280c9767ae5d741170740590e1d23d3da3
robert <robertrussell.72001@gmail.com>
2019-09-30 23:00:02 -0700

    Add more lexing test cases
    Mainly, add test cases to check when the lexer should and should not
    error.

--- lex_test.go
+++ lex_test.go
@@ -9,6 +9,11 @@ func TestLex(t *testing.T) {
 	}
 	for _, c := range []testCase{
 		{"a", []lexeme{{LTStatement, 'a'}}},
+		{"(a)", []lexeme{
+			{LTOpenParen, '('},
+			{LTStatement, 'a'},
+			{LTCloseParen, ')'},
+		}},
 		{"a > b", []lexeme{
 			{LTStatement, 'a'},
 			{LTOperator, '>'},
@@ -55,6 +60,38 @@ func TestLex(t *testing.T) {
 				t.Fatalf("expected %v; got %v", c.expected[idx], r.l)
 			}
 			idx++
+		}
+	}
+}
+
+func TestLexError(t *testing.T) {
+	type testCase struct {
+		input string
+		error bool
+	}
+	for _, c := range []testCase{
+		{"", true},
+		{"()", true},
+		{">", true},
+		{"(=)", true},
+		{"(", true},
+		{")", true},
+		{"a >", true},
+		{"^ a", true},
+		{"(a & b > c)", false},
+	} {
+		err := false
+		for r := range lex(c.input) {
+			if r.err != nil {
+				err = true
+			}
+		}
+		if err != c.error {
+			fill := ""
+			if !c.error {
+				fill = "not "
+			}
+			t.Fatalf("expected '%s' to %serror", c.input, fill)
 		}
 	}
 }
